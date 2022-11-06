abefddf7ca16d77a176c3360abaa701b9c0c3d17
Anton <krusinanton@gmail.com>
2022-11-06 17:54:14 +0100

    coordinate

--- coordinate/script.js
+++ coordinate/script.js
@@ -1,14 +1,18 @@
-let elem = document.querySelector('#field')
-function showXY() {
-    let r = elem.getBoundingClientRect()
-    let one = [r.left, r.top];
-    let two = [r.right, r.bottom];
-    let three = [r.left + field.clientLeft, r.top + field.clientTop];
-    let four = [r.left + field.clientLeft + field.clientWidth,
-                r.top + field.clientTop + field.clientHeight];
-    console.log(one)
-    console.log(two)
-    console.log(three)
-    console.log(four)
+// let elem = document.querySelector('#field')
+// function showXY() {
+//     let r = elem.getBoundingClientRect()
+//     let one = [r.left, r.top];
+//     let two = [r.right, r.bottom];
+//     let three = [r.left + field.clientLeft, r.top + field.clientTop];
+//     let four = [r.left + field.clientLeft + field.clientWidth,
+//                 r.top + field.clientTop + field.clientHeight];
+//     console.log(one)
+//     console.log(two)
+//     console.log(three)
+//     console.log(four)
+// }
+// showXY()
+
+hider.onclick = function invisible() {
+document.querySelector('#text').style.display = 'none';
 }
-showXY()
