198635a79752de0c4bdbb840efdf3bb46e8de754
Anton <krusinanton@gmail.com>
2022-11-06 16:24:12 +0100

    coordinate

---
+++ coordinate/script.js
@@ -0,0 +1,14 @@
+let elem = document.querySelector('#field')
+function showXY() {
+    let r = elem.getBoundingClientRect()
+    let one = [r.left, r.top];
+    let two = [r.right, r.bottom];
+    let three = [r.left + field.clientLeft, r.top + field.clientTop];
+    let four = [r.left + field.clientLeft + field.clientWidth,
+                r.top + field.clientTop + field.clientHeight];
+    console.log(one)
+    console.log(two)
+    console.log(three)
+    console.log(four)
+}
+showXY()
