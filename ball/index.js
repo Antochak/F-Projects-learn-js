5f08e296cd5cc51bf20096c92beda90b18fbec43
Anton <krusinanton@gmail.com>
2022-11-08 22:19:09 +0100

    ball

--- ball/index.js
+++ ball/index.js
@@ -8,14 +8,14 @@ field.addEventListener('click', (e) => {
     let newX = e.clientX - field.clientLeft - fieldRect.x - ballRadius;
     let newY = e.clientY - field.clientTop - fieldRect.y - ballRadius;
 
-    if (newX < 0) {
+    if (newX <= 0) {
         ball.style.left = "0px";
     } else if (newX + ball.offsetWidth > field.clientWidth) {
         ball.style.left = field.clientWidth - ball.offsetWidth + "px";
     } else { 
         ball.style.left = newX + "px";
     }
-        if (newY < 0) {
+        if (newY <= 0) {
             ball.style.top = "0px";
         } else if (newY + ball.offsetHeight > field.clientHeight) {
             ball.style.top = field.clientHeight - ball.offsetHeight + "px";
