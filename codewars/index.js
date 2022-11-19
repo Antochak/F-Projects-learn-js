a507faf8c844960c5a94dfbd199d0b855f036518
Anton <krusinanton@gmail.com>
2022-11-19 22:05:11 +0100

    first task

--- codewars/index.js
+++ codewars/index.js
@@ -246,9 +246,9 @@
 //         }
 //       console.log(makeNegative(7))
 
-
-function findShort(s){
-    let arr = s.split(" ").sort((a, b) => a.length - b.length);
-    return arr[0].length
-    }
-      console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
+// поиск самого короткого слова в массиве
+// function findShort(s){
+//     let arr = s.split(" ").sort((a, b) => a.length - b.length);
+//     return arr[0].length
+//     }
+//       console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
