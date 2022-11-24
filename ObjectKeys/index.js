bf10a7ceb32269fa9156165d619b918bba927495
Anton <krusinanton@gmail.com>
2022-11-24 17:48:45 +0100

    ObjectKeys_Values

---
+++ ObjectKeys/index.js
@@ -0,0 +1,24 @@
+// let salaries = {
+//     "John": 100,
+//     "Pete": 300,
+//     "Mary": 250
+//   };
+//   function sumSalries(salaries) {
+//   let sum = 0;
+//     for (let user of Object.values(salaries)) {
+//         sum = sum + user;
+//     }
+//     return sum
+// }
+// console.log(sumSalries(salaries))
+
+// let user = {
+//     name: 'John',
+//     age: 30
+//   };
+
+//   function count(user) {
+//     return Object.keys(user).length
+//   }
+//   console.log(count(user))
+
